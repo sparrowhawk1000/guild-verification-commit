8586cfe3eb48f07449916af776fc5bbfc9f88e9c
sparrowhawk1000 <166084284+sparrowhawk1000@users.noreply.github.com>
2026-03-07 01:08:26 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T22:00:52.812Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T22:08:26.277Z
+Commit: Fix broken links in documentation
