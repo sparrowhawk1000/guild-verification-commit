2bbd39db8b45b41826a95f4c57abf4f007b7003a
sparrowhawk1000 <166084284+sparrowhawk1000@users.noreply.github.com>
2026-03-07 01:00:51 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:59:01.954Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T22:00:51.085Z
+Commit: Fix broken links in documentation
